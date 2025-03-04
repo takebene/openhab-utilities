79d457f1d5951d6f0674e20faed9ce0c2a2a2e3a
Linke, Benedikt <benedikt.linke@4soft.de>
2025-03-04 09:30:38 +0100

    improve controlGarageDoor

--- garagedoorUtils.js
+++ garagedoorUtils.js
@@ -1,23 +1,25 @@
 function controlGarageDoor(
   event,
-  entdSwitchOpen,
+  endSwitchOpen,
   endSwitchClosed,
   currentState,
   targetState
 ) {
-  if (entdSwitchOpen.state === "OPEN" && endSwitchClosed.state === "OPEN") {
+  if (endSwitchOpen.state === "OPEN" && endSwitchClosed.state === "OPEN") {
     var state = targetState.state == 0 ? "OPENING" : "CLOSING";
     currentState.postUpdate(state);
     console.info("Garagedoor is " + state);
   }
 
-  if (entdSwitchOpen.state === "CLOSED") {
+  if (endSwitchOpen.state === "CLOSED") {
     currentState.postUpdate("OPEN");
+    targetState.postUpdate(0);
     console.info("Garagedoor is OPEN");
   }
 
   if (endSwitchClosed.state === "CLOSED") {
     currentState.postUpdate("CLOSED");
+    targetState.postUpdate(1);
     console.info("Garagedoor is CLOSED");
   }
 }
